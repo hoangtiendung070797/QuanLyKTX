a4fb564cd3b4e866099fd3c148c8cb50ed1fefd6
Admin <Admin@DESKTOP-47ALEVC>
2019-03-24 23:11:15 +0700

    Xong doi tuong, day nha

---
+++ BUS/BUS_DayNha.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BUS
+{
+    public class BUS_DayNha
+    {
+        DAL_DayNha dayNha = new DAL_DayNha();
+        public bool Insert(DayNha dayNha)
+        {
+            return this.dayNha.Insert(dayNha);
+        }
+
+        public bool Delete(int dayNhaId)
+        {
+            return this.dayNha.Delete(dayNhaId);
+        }
+
+        public bool Update(DayNha dayNha)
+        {
+            return this.dayNha.Update(dayNha);
+        }
+
+    }
+}
