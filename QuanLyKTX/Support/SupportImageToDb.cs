e16b3808e0464d0eef0e32dc928c980f7f642a82
ThanHoang <ThanHoang@DESKTOP-K0913AQ>
2019-03-29 22:23:18 +0700

    suport image

---
+++ QuanLyKTX/Support/SupportImageToDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace QuanLyKTX.Support
+{
+    public class SupportImageToDb
+    {
+        private string ImageToBytes;
+
+        public string ImageToBytes1 { get => ImageToBytes; set => ImageToBytes = value; }
+
+        public byte[] converImgToByte(string path)
+        {
+            FileStream fs;
+            fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] picbyte = new byte[fs.Length];
+            fs.Read(picbyte, 0, System.Convert.ToInt32(fs.Length));
+            fs.Close();
+            return picbyte;
+        }
+
+        public string GetPathImage()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Pictures files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png)|*.jpg; *.jpeg; *.jpe; *.jfif; *.png|All files (*.*)|*.*";
+            openFile.FilterIndex = 1;
+            openFile.RestoreDirectory = true;
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                return openFile.FileName;
+            }
+            return "";
+        }
+
+        public Image ByteToImg(string byteString)
+        {
+            byte[] imgBytes = Convert.FromBase64String(byteString);
+            MemoryStream ms = new MemoryStream(imgBytes, 0, imgBytes.Length);
+            ms.Write(imgBytes, 0, imgBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
+
+    }
+}
